021a95ec1e78beded2a48eb7dbb199ade5cc8faa
Raksmey <raksmeysu34@gmail.com>
2026-09-14 12:28:54 +0700

    First

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name" : "Khmer Reports",
+    "summary": """
+            This addon will add 4 new print options on Purchase and Inventory APP
+        """,
+    "version": "19.0.1.0.0",
+    "author": "Raksmey",
+    "license": "AGPL-3",
+    "website": "",
+    "category": "Purchase/Localizations and Inventory/Localizations",
+    "depends": ["purchase", "stock", "purchase_request", "report_layout_template"],
+    "data": [
+        'report/report_purchase_request.xml',
+        'report/report_purchase_order.xml',
+        'report/report_stock_receipt.xml',
+        'report/report_stock_delivery.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
